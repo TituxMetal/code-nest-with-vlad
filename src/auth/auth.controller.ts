88f92852e74327a2744c052648b8f1d077e2d1f6
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-15 06:41:01 +0200

    feat: Refactor signup and login endpoints in AuthController
    This commit refactors the signup and login endpoints in the AuthController to improve code readability and maintainability. It introduces the use of the UserEntity class for handling user data storage and adds type annotations to the signup and login methods.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,23 @@ import { Body, Controller, Post } from '@nestjs/common'
 
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto'
+import { UserEntity } from './entity'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authSerivce: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() authDto: AuthDto) {
-    return await this.authSerivce.signup(authDto)
+  async signup(@Body() authDto: AuthDto): Promise<UserEntity> {
+    const { id, email } = await this.authSerivce.signup(authDto)
+
+    return new UserEntity({ id, email })
   }
 
   @Post('login')
-  async login(@Body() authDto: AuthDto) {
-    return this.authSerivce.login(authDto)
+  async login(@Body() authDto: AuthDto): Promise<UserEntity> {
+    const { id, email } = await this.authSerivce.login(authDto)
+
+    return new UserEntity({ id, email })
   }
 }
