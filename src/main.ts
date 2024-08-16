8984096afe514f134c9364b5e8b802d76e506a6e
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-16 10:32:45 +0200

    feat: Add session middleware for session management
    This commit adds the session middleware to the main.ts file in order to enable session management in the application. The session middleware is configured with the session secret obtained from the ConfigService. This change improves the security and functionality of the application by allowing the storage and retrieval of session data for authenticated users.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { type NestExpressApplication } from '@nestjs/platform-express'
+import * as session from 'express-session'
 
 import { AppModule } from '~/app.module'
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const config = app.get(ConfigService)
+  const sessionSecret = config.getOrThrow<string>('SESSION_SECRET')
+
+  app.use(
+    session({ secret: sessionSecret, resave: false, saveUninitialized: false })
+  )
 
   await app.listen(3000)
   console.log(`Application is running on: http://localhost:3000`)
