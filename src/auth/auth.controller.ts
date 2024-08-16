0091eb237e534c7ea6c4abda0f4ffdbd5cb696d9
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-16 10:37:41 +0200

    feat: Update signup and login endpoints in AuthController to include sessions.
    - Add the user id and the email in the session

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,50 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Session
+} from '@nestjs/common'
 
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto'
 import { UserEntity } from './entity'
+import { UserSession, UserSessionData } from './types'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authSerivce: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() authDto: AuthDto): Promise<UserEntity> {
+  async signup(
+    @Body() authDto: AuthDto,
+    @Session() session: UserSession
+  ): Promise<UserEntity> {
     const { id, email } = await this.authSerivce.signup(authDto)
+
+    this.serializeSession({ id, email }, session)
 
     return new UserEntity({ id, email })
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
-  async login(@Body() authDto: AuthDto): Promise<UserEntity> {
-    const { id, email } = await this.authSerivce.login(authDto)
+  async login(
+    @Body() authDto: AuthDto,
+    @Session() session: UserSession
+  ): Promise<UserEntity> {
+    const user = await this.authSerivce.login(authDto)
 
-    return new UserEntity({ id, email })
+    this.serializeSession({ ...user }, session)
+
+    return new UserEntity(user)
+  }
+
+  private serializeSession(
+    userData: UserSessionData,
+    session: UserSession
+  ): void {
+    session.user = { ...userData }
   }
 }
