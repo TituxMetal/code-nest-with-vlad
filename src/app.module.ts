cdfe2fee996984f286d2cc18574a2a943aae9d20
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-18 11:53:43 +0200

    feat: Add user module and session guard provider to the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
 
-import { AuthModule } from '~/auth'
+import { AuthModule, SessionGuard } from '~/auth'
 import { PrismaModule } from '~/prisma'
+import { UserModule } from '~/user'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     AuthModule,
-    PrismaModule
+    PrismaModule,
+    UserModule
   ],
   controllers: [],
-  providers: []
+  providers: [{ provide: APP_GUARD, useClass: SessionGuard }]
 })
 export class AppModule {}
