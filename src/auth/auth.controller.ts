c5114e201cc66e5b6f23a570271311c822aba0d2
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-18 11:47:27 +0200

    feat: Add PublicRoute decorator in AuthController
    This commit adds the PublicRoute decorator to mark routes as public, allowing them to be accessed without authentication. This change improves the codebase by providing a convenient way to handle public routes in the authentication system.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,10 +8,12 @@ import {
 } from '@nestjs/common'
 
 import { AuthService } from './auth.service'
+import { PublicRoute } from './decorators'
 import { AuthDto } from './dto'
 import { UserEntity } from './entity'
 import { UserSession, UserSessionData } from './types'
 
+@PublicRoute()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authSerivce: AuthService) {}
