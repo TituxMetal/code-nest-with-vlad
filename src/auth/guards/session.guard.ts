d080a69533562c8f4dbde07bd67684b229cb464a
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-18 11:42:47 +0200

    feat: Add SessionGuard for authentication
    The commit adds a new file `session.guard.ts` in the `auth/guards` directory. This file contains the implementation of the `SessionGuard` class, which is responsible for handling authentication for routes that require a session. The `SessionGuard` class implements the `CanActivate` interface and checks if the route is marked as public using the `IS_PUBLIC_ROUTE` decorator. If the route is not public, it checks if the user session exists and throws an `UnauthorizedException` if it doesn't. This change improves the codebase by providing a centralized way to handle session-based authentication for protected routes.

---
+++ src/auth/guards/session.guard.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  type CanActivate,
+  type ExecutionContext
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { type Request } from 'express'
+import { type Observable } from 'rxjs'
+
+import { IS_PUBLIC_ROUTE } from '~/auth/decorators'
+import { type UserSession } from '~/auth/types'
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublicRoute = this.reflector.getAllAndOverride<string>(
+      IS_PUBLIC_ROUTE,
+      [context.getHandler(), context.getClass()]
+    )
+
+    if (isPublicRoute) {
+      return true
+    }
+
+    const request = context.switchToHttp().getRequest() as Request
+    const session = request.session as UserSession
+
+    if (!session.user) {
+      throw new UnauthorizedException('Session not provided')
+    }
+
+    return true
+  }
+}
