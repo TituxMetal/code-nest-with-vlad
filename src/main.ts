9582cd0eb232d650e76981f55ccca750e41d286b
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-20 05:23:48 +0200

    feat: Add Redis session store for improved session management

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { type NestExpressApplication } from '@nestjs/platform-express'
+import * as connectRedis from 'connect-redis'
 import * as session from 'express-session'
+import { createClient } from 'redis'
 
 import { AppModule } from '~/app.module'
 
@@ -10,9 +12,24 @@ const bootstrap = async () => {
   const config = app.get(ConfigService)
   const sessionSecret = config.getOrThrow<string>('SESSION_SECRET')
 
+  const RedisStore = connectRedis(session)
+  const redisClient = createClient({
+    url: config.getOrThrow<string>('REDIS_URL'),
+    legacyMode: true
+  })
+
   app.use(
-    session({ secret: sessionSecret, resave: false, saveUninitialized: false })
+    session({
+      secret: sessionSecret,
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisStore({ client: redisClient })
+    })
   )
+
+  await redisClient.connect().catch(error => {
+    throw error
+  })
 
   await app.listen(3000)
   console.log(`Application is running on: http://localhost:3000`)
