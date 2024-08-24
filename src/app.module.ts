9f5fc4989197ddb9e93c864f8ea39d34e219f7c3
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-24 11:24:51 +0200

    chore: Add Redis cache module and configuration
    This commit adds the Redis cache module and configuration to the app module. This change improves the app's performance by implementing caching with Redis.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+import { CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
+import * as redisStore from 'cache-manager-redis-store'
+import { RedisClientOptions } from 'redis'
 
 import { AuthModule, SessionGuard } from '~/auth'
 import { PrismaModule } from '~/prisma'
@@ -9,6 +12,15 @@ import { UserModule } from '~/user'
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        store: redisStore,
+        url: config.getOrThrow<string>('REDIS_URL'),
+        ttl: 600 // 10 minutes in seconds
+      })
+    }),
     AuthModule,
     PrismaModule,
     UserModule
